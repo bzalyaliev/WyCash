95051f6b020bf1204487bea4f8022c46d7c2db5f
Bulat Zalyaliev <bzalyaliev@gmail.com>
2023-02-18 20:00:55 +0300

    Added Franc class and test for Franc multiplication

---
+++ src/main/java/com/github/bzalyaliev/wycash/Franc.java
@@ -0,0 +1,18 @@
+package com.github.bzalyaliev.wycash;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return this.amount == franc.amount;
+    }
+}
